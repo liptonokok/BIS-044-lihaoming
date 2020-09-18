73952da03ba9edc015df902dab31c8df1e19996c
liptonokok <hal223@lehigh.edu>
2020-09-18 20:17:50 +0800

    CReated Assignment 02B

---
+++ Assignment 02B.R
@@ -0,0 +1,24 @@
+# Data Frames
+x <- 10:1
+y <- -4:5
+z <- c('Hockey','Football','Curling', 'Soccer', 'Rugby', 'Baseball', 'Golf', 'Basketball', 'Wrestling', 'Tennis')
+theDF <- data.frame(x, y, z)
+theDF
+
+str(theDF)
+
+theDF [1,2]
+View(theDF)
+
+# Using names
+theDF$x
+theDF$x[3]
+
+# Editing names
+names(theDF) <- c("Popularity", "Team Strength", "Sport")
+View(theDF)
+
+# Computing sum
+Pop <- sum(theDF[1,1]:theDF[10,1])
+Pop
+cat("The sum of popularity is", Pop)
